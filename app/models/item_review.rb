e55f65244096a4f4d370984fef1cb841edc07457
Greg Kulasik <gkulasik@gmail.com>
2014-11-07 05:13:38 +0000

    fix syntax errors

--- app/models/item_review.rb
+++ app/models/item_review.rb
@@ -1,6 +1,5 @@
 class ItemReview < ActiveRecord::Base
-	belongs_to :Item
-	add_index: :item :Item_ID
+	belongs_to :item
 	validates :rating, presence: true,
 		numericality:{only_integer:true,
 		greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
