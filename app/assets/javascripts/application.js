44d4007062fb33ff36e27dbb67a45340d67557c9
Greg Kulasik <gkulasik@gmail.com>
2014-11-11 20:28:51 +0000

    disable form on checkbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,31 @@
 //= require turbolinks
 //= require_tree .
 
+$("input[type='checkbox']").click(function () {
+    var thisCheckbox = $(this);
+    var id = $(this).attr("id");
+//     var text = $("label[for=" + id + "]").text().toLowerCase();
+//     var name = $(this).attr("name").replace("[" + text + "]", "");
+  var array_names = ["shipping_address", "shipping_city", "shipping_postal_code", "shipping_state_or_province", "shipping_country"]
+  for(i=0; i < 4; i++){
+  $("input[name*='" + "checkout["+ array_names[i] + "]"+ "']").each(function () {
+        var thisInput = $(this);
+        if (thisInput.attr("disabled")) {
+            thisInput.removeAttr("disabled");
+        } else {
+            thisInput.attr("disabled", "disabled");
+            thisCheckbox.removeAttr("disabled");
+        }
+  })}
+  $("select[name*='" + "checkout["+ array_names[array_names.length-1] + "]"+ "']").each(function () {
+        var thisInput = $(this);
+        if (thisInput.attr("disabled")) {
+            thisInput.removeAttr("disabled");
+        } else {
+            thisInput.attr("disabled", "disabled");
+            thisCheckbox.removeAttr("disabled");
+        }
+  })
+});
+
 $(function(){ $(document).foundation(); });
